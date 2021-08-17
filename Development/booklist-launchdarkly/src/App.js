588914458cd9a193cec0d8707d8388792b5f086f
ericabasak <ericabasak@gmail.com>
2021-08-16 22:01:57 -0400

    first commit for this mini project to practice feature flags launchdarkly

---
+++ Development/booklist-launchdarkly/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>Book Title 1</li>
+        <li>Book Title 2</li>
+        <li>Book Title 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
